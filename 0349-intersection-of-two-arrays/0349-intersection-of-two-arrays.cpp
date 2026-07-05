3d1083320494aef40293499df326de9bb97b8ed1
KUMAR YASHANSH <kr.yashansh123@gmail.com>
2026-07-05 22:23:23 +0530

    Time: 7 ms (10.44%), Space: 14.5 MB (37.78%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,20 +1,36 @@
+// class Solution {
+// public:
+//     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+//         int n = nums1.size();
+//         int c = nums2.size();
+//         vector<int> a;
+//         for(int i = 0;i<n;i++){
+//             for(int j = 0;j<c;j++){
+//                 if(nums1[i] == nums2[j]){
+//                     a.push_back(nums1[i]);
+                    
+
+//                 }
+//             }
+
+//         }
+//         unordered_set<int> s(a.begin(), a.end()); 
+//         return vector<int>(s.begin(), s.end());  
+//     }
+// };
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int c = nums2.size();
-        vector<int> a;
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<c;j++){
-                if(nums1[i] == nums2[j]){
-                    a.push_back(nums1[i]);
-                    
 
-                }
+        unordered_set<int> s1(nums1.begin(), nums1.end());
+        unordered_set<int> ans;
+
+        for(int x : nums2) {
+            if(s1.count(x)) {
+                ans.insert(x);
             }
-
         }
-        unordered_set<int> s(a.begin(), a.end()); 
-        return vector<int>(s.begin(), s.end());  
+
+        return vector<int>(ans.begin(), ans.end());
     }
 };
