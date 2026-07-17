676fba84f5502398ed0f713222ce0998bad31b52
KUMAR YASHANSH <kr.yashansh123@gmail.com>
2026-07-17 13:22:45 +0530

    Time: 5 ms (64.48%), Space: 128.3 MB (54.37%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,30 +1,54 @@
 
 // BRUTE FORCE
+// class Solution {
+// public:
+//     vector<int> rearrangeArray(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> pos;
+//         vector<int> neg;
+//         vector<int> fi(n);
+
+//         for(int i = 0;i<n;i++){
+//             if(nums[i] > 0){
+//                 pos.push_back(nums[i]);
+//             }else{
+//                 neg.push_back(nums[i]);
+//             }
+//         }
+//         for(int i = 0; i < pos.size(); i++) {
+//         fi[2 * i] = pos[i];
+//         fi[2 * i + 1] = neg[i];
+// }
+//         return fi;
+
+        
+//     }
+// };
+// // OPTIMAL CODE
+
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
         int n = nums.size();
-        vector<int> pos;
-        vector<int> neg;
-        vector<int> fi(n);
-
-        for(int i = 0;i<n;i++){
-            if(nums[i] > 0){
-                pos.push_back(nums[i]);
+        vector<int>ans(n);
+        int pos=0;
+        int neg=1;
+        for(int x:nums){
+            if(x>0){
+                ans[pos]=x;
+                pos+=2;
             }else{
-                neg.push_back(nums[i]);
+                ans[neg]=x;
+                neg+=2;
             }
         }
-        for(int i = 0; i < pos.size(); i++) {
-        fi[2 * i] = pos[i];
-        fi[2 * i + 1] = neg[i];
-}
-        return fi;
-
+        return ans;
         
     }
 };
-// OPTIMAL CODE
+
+
+
 
 
 
