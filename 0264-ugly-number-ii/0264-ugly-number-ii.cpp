4283bcfaa00660402581f3bc47e855de0e706999
KUMAR YASHANSH <kr.yashansh123@gmail.com>
2026-07-31 01:42:32 +0530

    Time: 3 ms (79.08%), Space: 11.3 MB (67.37%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        vector<int> ugly(n);
+        ugly[0] = 1;
+        
+        int p2 = 0, p3 = 0, p5 = 0;
+
+        for (int i = 1; i < n; i++) {
+            int next2 = ugly[p2] * 2;
+            int next3 = ugly[p3] * 3;
+            int next5 = ugly[p5] * 5;
+
+            int nextUgly = min({next2, next3, next5});
+            ugly[i] = nextUgly;
+
+            if (nextUgly == next2) p2++;
+            if (nextUgly == next3) p3++;
+            if (nextUgly == next5) p5++;
+        }
+
+        return ugly[n - 1];
+    }
+};
