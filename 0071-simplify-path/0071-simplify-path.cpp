ab516403aef15db4b5600cb563da2f105f601c5f
KUMAR YASHANSH <kr.yashansh123@gmail.com>
2026-08-06 13:43:05 +0530

    Time: 3 ms (63.29%), Space: 11.2 MB (59.08%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> st;
+        stringstream ss(path);
+        string token;
+        
+        while (getline(ss, token, '/')) {
+            if (token == "" || token == ".") {
+                continue;
+            } else if (token == "..") {
+                if (!st.empty()) {
+                    st.pop_back();
+                }
+            } else {
+                st.push_back(token);
+            }
+        }
+        
+        string res = "";
+        for (const string& dir : st) {
+            res += "/" + dir;
+        }
+        
+        return res.empty() ? "/" : res;
+    }
+};
