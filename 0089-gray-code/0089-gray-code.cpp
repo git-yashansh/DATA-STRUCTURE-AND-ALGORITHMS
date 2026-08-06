3fed3dff9b4b3797abd0038b86185cd8950e527b
KUMAR YASHANSH <kr.yashansh123@gmail.com>
2026-08-06 21:33:13 +0530

    Time: 0 ms (100%), Space: 14 MB (94.34%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> result;
+        int size = 1 << n;
+        result.reserve(size);
+        for (int i = 0; i < size; ++i) {
+            result.push_back(i ^ (i >> 1));
+        }
+        return result;
+    }
+};
