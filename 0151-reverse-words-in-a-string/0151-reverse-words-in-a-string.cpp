898557e59ba3cddd44b0609b09e0f87272cbd040
KUMAR YASHANSH <kr.yashansh123@gmail.com>
2026-08-19 18:06:24 +0530

    Time: 1 ms (45.38%), Space: 11.4 MB (18.17%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -6,16 +6,17 @@ public:
         vector <string> ans;
         while(ss >> word){
             ans.push_back(word);
+
         }
         reverse(ans.begin(),ans.end());
         string ans2;
-        for(int i=0;i<ans.size();i++){
-            if(i>0){
-                ans2+=" ";
+        for(int i = 0;i<ans.size();i++){
+            if(i > 0){
+                ans2 += " ";
             }
             ans2 += ans[i];
         }
-        
         return ans2;
+
     }
 };
