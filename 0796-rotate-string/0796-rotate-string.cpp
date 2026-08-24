eacac2b2737d943ec80b9bc9ff80da7e4cd7b79b
KUMAR YASHANSH <kr.yashansh123@gmail.com>
2026-08-24 21:25:27 +0530

    Time: 0 ms (100%), Space: 7.9 MB (97.36%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n = s.size();
+        if(s.size() != goal.size()){
+            return false;
+        }
+        // d = d % s.size();
+        for (int i = 0; i <= n; ++i) {
+            rotate(s.begin(), s.begin() + 1, s.end());
+            if(s == goal){
+            return true;
+            }
+        }
+        return false;
+
+        
+    }
+};
