e40a03123b13b9f2ce3dc0e3ab747e5dd92df0e7
KUMAR YASHANSH <kr.yashansh123@gmail.com>
2026-08-26 16:06:55 +0530

    Time: 0 ms (100%), Space: 11.4 MB (38.08%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -5,7 +5,9 @@ public:
         for(char c:s){
             mp[c]++;
         }
+        // jo freq map ho chuka hai usko vector banaye h taki sort kr sake
         vector<pair<char,int>>v(mp.begin(),mp.end());
+        // lamda fubction use kiye h u=kyuki mere hisab se sort krna hai isliye
         sort(v.begin(),v.end(),[](auto &a, auto &b){
             return a.second > b.second;
         });
